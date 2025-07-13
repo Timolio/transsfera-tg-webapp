cf91c8c9f10e3ad6dab14ea3e55d907ed52b04b3
Timolio <94037690+Timolio@users.noreply.github.com>
2025-07-13 20:01:25 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup>
+useHead({
+    script: [
+        {
+            src: 'https://telegram.org/js/telegram-web-app.js',
+            defer: true,
+        },
+    ],
+});
+</script>
+
+<template>
+    <main>
+        <ClientOnly>
+            <slot />
+        </ClientOnly>
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { useWebApp } = await import('vue-tg');
+
+const { sendData } = useWebApp();
+
+const info = ref('');
+
+const confirm = () => {
+    sendData(info.value);
+};
+</script>
+
+<template>
+    <div>
+        <input type="text" v-model="info" placeholder="Enter some info" />
+        <button @click="confirm">Confirm</button>
+    </div>
+</template>
